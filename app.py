718012866d744810872f1224216c96b7def1e78a
haseebuchiha <haseeb9009@gmail.com>
2019-02-26 19:47:48 +0500

    Math functions and static variables

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-x = 10 + 3
-x = 10-3
-x = 30*4
-x = 10//3
-x = 10/3
-x = 10 % 3
-x = 10**3
+import math
 
-x = x+1
-x += 1
-
-print(x)
-
-# No increment operators such as x++ or x--
+# Static variable are shown as Upper case but there are no static variables in python
+PI = -3.14
+print(round(PI))
+print(abs(PI))
+print(math.floor(PI))
