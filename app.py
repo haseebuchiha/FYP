2c4ac45f703e865d6fe02503768b34fe2e1b2aaf
haseebuchiha <haseeb9009@gmail.com>
2019-02-26 21:16:42 +0500

    Fizz buzz problem :D Solved!

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-def multiply(*list):
-    print(list)
+# if divisble by 3 fizz
+# if divisible by 5 buzz
+# if divisible by 3 and 5 fizz buzz
+# else return itself
 
 
-multiply(2, 3, 4, 5)
+def fizz_buzz(input: int):
+    if input % 3 == 0 and input % 5 == 0:
+        print("Fizz Buzz")
+    elif input % 3 == 0:
+        print("Fizz ")
+    elif input % 5 == 0:
+        print("Buzz")
+    else:
+        print(input)
+
+
+x = int(input("Enter value: "))
+fizz_buzz(x)
