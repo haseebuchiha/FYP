397133c778117fe7f48170a9a793a2fecf57252a
haseebuchiha <haseeb9009@gmail.com>
2019-02-26 20:19:15 +0500

    if Statements

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-x = input("x: ")
-# y = x+1
+age = 22
 
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("Child")
 
-print(int(x))
-print(float(x))
-print(bool(x))
-# Falsy Statements
-# ""
-# 0
-# []
-# None(Null)
+print("All Done")
+
+if age > 1:
+    pass
+else:
+    pass
