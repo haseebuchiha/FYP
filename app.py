291efce50517f57721625884ed38ec2d252301c3
haseebuchiha <haseeb9009@gmail.com>
2019-02-26 21:21:39 +0500

    Fizz_Buzz better practices solution :(

--- app.py
+++ app.py
@@ -5,15 +5,15 @@
 
 
 def fizz_buzz(input: int):
-    if input % 3 == 0 and input % 5 == 0:
-        print("Fizz Buzz")
-    elif input % 3 == 0:
-        print("Fizz ")
-    elif input % 5 == 0:
-        print("Buzz")
-    else:
-        print(input)
+    if (input % 3 == 0) and (input % 5 == 0):
+        return "Fizz Buzz"
+    if input % 3 == 0:
+        return "Fizz"
+    if input % 5 == 0:
+        return "Buzz"
+
+    return input
 
 
 x = int(input("Enter value: "))
-fizz_buzz(x)
+print(fizz_buzz(x))
